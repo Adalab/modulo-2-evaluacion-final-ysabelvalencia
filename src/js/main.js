bde39a26a0cac42910ba50268fc45142dc075e98
ysabelvalencia <ysavalencia@hotmail.com>
2023-09-28 22:34:59 +0200

    js query selectors

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+//query selectors, arrays vacios
+
+const searchText = document.querySelector('.js-input-search');
+
+const btnSearch = document.querySelector('.js-btn-search');
+
+const showContainer = document.querySelector('.js-list-favorites');
+
+let showList = [];
