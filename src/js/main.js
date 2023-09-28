685ee750f84d7e9c6a57eedc2d925add24d069a8
ysabelvalencia <ysavalencia@hotmail.com>
2023-09-28 23:00:16 +0200

    function getApi

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,28 @@ const btnSearch = document.querySelector('.js-btn-search');
 const showContainer = document.querySelector('.js-list-favorites');
 
 let showList = [];
+
+function renderShow(oneShow) {
+  let html = '';
+  html += ``;
+}
+
+//funciones manejadoras
+function handleClickSearch(ev) {
+  ev.preventDefault();
+  getApi();
+}
+
+function getApi() {
+  const inputValue = searchText.value;
+  fetch(`//api.tvmaze.com/search/shows?q=${inputValue}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      showList = data.show;
+    });
+}
+
+//Eventos
+
+btnSearch.addEventListener('click', handleClickSearch);
